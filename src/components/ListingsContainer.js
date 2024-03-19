94942a56d37bf2a2b5f00c9745cc8c996702c3f3
Tejas Nampalli <TSNampalli@gmail.com>
2024-03-19 16:42:41 -0400

    c

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then(r =>r.json())
+    .then(listings => setListings(listings))
+  }, [])
+
+function handleDeleteListing(id) {
+  const updatedListingsArray = listings.filter(listing => listing.id !== id)
+  setListings(updatedListingsArray)
+}
+
+const filteredListings = listings.filter(listing => {
+  return listing.description.toLowerCase().includes(search.toLowerCase())
+})
+
+console.log(filteredListings)
+
+  const listingsCards = filteredListings.map(listingObj => {
+    return <ListingCard 
+    key={listingObj.id} 
+    listing={listingObj}
+    handleDeleteListing={handleDeleteListing}/>
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsCards}
       </ul>
     </main>
   );
